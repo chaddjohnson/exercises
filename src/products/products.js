d7c92e86065dd9a746898d4c029051d48eaf94ce
Chad Johnson <chad.d.johnson@gmail.com>
2015-08-29 03:14:48 -0500

    Fixed spacing.

--- src/products/products.js
+++ src/products/products.js
@@ -1,7 +1,8 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('.products').on('click', '.delete', function(event) {
         event.preventDefault();
-        if(confirm('Are you sure you want to delete this product?')) {
+
+        if (confirm('Are you sure you want to delete this product?')) {
             alert('deleting product');
         };
     });
