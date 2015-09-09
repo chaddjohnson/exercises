d89ccb4a8f780af78d953605aa55475db82a7781
sanskrtapanditah <c.scott.johnson@gmail.com>
2015-09-09 07:03:39 +0700

    Revised indentation in products.js

--- src/app/modules/products.js
+++ src/app/modules/products.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
     // Get data for products from /mocks/products.json via AJAX
     $.get('/mocks/products.json', function(response) {
-        // Populate the 'products' table's tbody elementent with tr elementents based on the response data array.
         var tableBody = $('#products tbody');
-    $.each(response, function(index, element){
-        tableBody.append(
-        	'<tr>' + 
-        	'<td>' + element._id + '</td>' +
-        	'<td>' + element.name + '</td>' +
-        	'<td>' + element.quantity + '</td>' +
-        	'<td>' + element.price + '</td>' +
-        	'<td><a href="/products/1">Edit</a></td>' +
-        	'<td><a class="delete" href="#">Delete</a></td>' +
-        	'</tr>'
+
+        // Populate the 'products' table's tbody elementent with tr elementents based on the response data array.
+        $.each(response, function(index, element){
+            tableBody.append(
+        	    '<tr>' +
+        	    '<td>' + element._id + '</td>' +
+        	    '<td>' + element.name + '</td>' +
+        	    '<td>' + element.quantity + '</td>' +
+        	    '<td>' + element.price + '</td>' +
+        	    '<td><a href="/products/1">Edit</a></td>' +
+        	    '<td><a class="delete" href="#">Delete</a></td>' +
+        	    '</tr>'
         	);
         });
     });               
