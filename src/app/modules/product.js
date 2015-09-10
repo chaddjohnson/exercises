3e4afc619909a6525431cccb0f96e9c6dfc9217e
sanskrtapanditah <c.scott.johnson@gmail.com>
2015-09-10 12:50:35 +0700

    Fixed product.js

--- src/app/modules/product.js
+++ src/app/modules/product.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
     //Determine which product is being edited based on the URL.
     var url = window.location.pathname;
-    var productId = url.substring(url.lastIndexOf('/') +);
+    var productId = url.substring(url.lastIndexOf('/') +1);
 
     //Get data for the product.
     $.get('/mocks/products/' + productId + '.json', function(response) {
-        console.log(response);
-        var templateFN = Handlebars.getTemplate('products/edit.hbs');
+        var templateFn = Handlebars.getTemplate('products/edit.hbs');
         var templateData = response;
 
         $('main').html(templateFn(templateData));        
